8e44e7a74ad055d255b4f74d121e89b186400d4c
Daizaikun <leo-im@hotmail.com>
2023-01-23 02:02:31 -0500

    finish

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 import sharp from "sharp";
 import fs from "node:fs";
 
-export function converter (resize=(500, 500), pathSVG= "./svg/", pathImg= "./img/"){
-const files = fs.readdirSync(pathSVG);
+export function converter(
+  resize = (500, 500),
+  pathSVG = "./svg/",
+  pathImg = "./img/"
+) {
+  const files = fs.readdirSync(pathSVG);
+  console.log(files);
 
-console.log(files);
+  files.map((file) => {
+    console.log(file);
 
-files.map((file) => {
-  console.log(file);
+    const name = file.replace(".svg", ".png");
 
-  const name = file.replace(".svg", ".png");
-
-  sharp(pathSVG + file)
-    .png()
-    .resize(resize)
-    .toFile(pathImg + name)
-    .then(function (info) {
-      console.log(info);
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
-});}
-
+    sharp(pathSVG + file)
+      .png()
+      .resize(resize)
+      .toFile(pathImg + name)
+      .then(function (info) {
+        console.log(info);
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  });
+}
